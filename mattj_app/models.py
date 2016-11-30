ec89f086eeb351366f63e080ff44d8f8de277ee4
Matt J <mattjone3@gmail.com>
2016-11-29 17:39:12 -0800

    include project experiences

--- mattj_app/models.py
+++ mattj_app/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Experience(models.Model):
-    exp_type = models.CharField(default = 'professional', max_length = 50, choices = [('education','Education'),('professional','Professional')])
+    exp_type = models.CharField(default = 'professional', max_length = 50, choices = [('education','Education'),('professional','Professional'),('Project','project')])
     start_date = models.DateField(blank = False)
     end_date = models.DateField(null = True)
     employer = models.CharField(max_length = 30)
