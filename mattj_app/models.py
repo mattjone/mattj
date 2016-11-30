7a866dd5df3b44fb2de8c3cd42dcb4be6241dfb6
Matt Jones <Matt@DN0a63283c.SUNet>
2016-11-29 17:31:50 -0800

    create and migrate Experience model

--- mattj_app/models.py
+++ mattj_app/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Experience(models.Model):
+    exp_type = models.CharField(default = 'professional', max_length = 50, choices = [('education','Education'),('professional','Professional')])
+    start_date = models.DateField(blank = False)
+    end_date = models.DateField(null = True)
+    employer = models.CharField(max_length = 30)
+    position = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 1000)
