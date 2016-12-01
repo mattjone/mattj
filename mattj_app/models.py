c1234a6897a68138b205eec221b18b1b1620cf7a
Matt J <mattjone3@gmail.com>
2016-12-01 12:27:37 -0800

    various database updates and changes to the content / structural page changes

--- mattj_app/models.py
+++ mattj_app/models.py
@@ -3,9 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Experience(models.Model):
-    exp_type = models.CharField(default = 'professional', max_length = 50, choices = [('education','Education'),('professional','Professional'),('Project','project')])
+    exp_type = models.CharField(default = 'professional', max_length = 30, choices = [('education','Education'),('professional','Professional'),('Project','project')])
     start_date = models.DateField(blank = False)
-    end_date = models.DateField(null = True)
-    employer = models.CharField(max_length = 30)
-    position = models.CharField(max_length = 50)
+    end_date = models.DateField(blank = True, null = True)
+    employer = models.CharField(max_length = 100)
+    position = models.CharField(max_length = 100)
     description = models.CharField(max_length = 1000)
+
+    def __str__(self):
+        return "%s: %s" % (self.employer, self.position)
--- mattj_app/views.py
+++ mattj_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from mattj_app.models import Experience
 
 def home(request):
-    return render(request, 'home.html',{})
+    prof_exp = Experience.objects.filter(exp_type = 'professional').order_by("-start_date")
+    ed_exp = Experience.objects.filter(exp_type = 'education').order_by("-start_date")
+    return render(request, 'home.html',{'professional_experience':prof_exp,'education_experience':ed_exp})
